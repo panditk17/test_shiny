0160cc076470432cc806aef20b951c64ded3f26a
Pandit <karunpandit@ufl.edu>
2022-06-01 23:57:25 -0400

    create codes

---
+++ test_scripts_1.R
@@ -0,0 +1,25 @@
+## codes to test r shiny app
+
+library(shiny)
+ui<-fluidPage()
+
+server <- function(input, output) {
+  
+  selectedData <- reactive({
+    iris[, c(input$xcol, input$ycol)]
+  })
+  
+  clusters <- reactive({
+    kmeans(selectedData(), input$clusters)
+  })
+  
+  output$plot1 <- renderPlot({
+    par(mar = c(5.1, 4.1, 0, 1))
+    plot(selectedData(),
+         col = clusters()$cluster,
+         pch = 20, cex = 3)
+    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+  })}
+
+server<-fuction(input,output){}
+shinyApp(ui=ui,server=server)
