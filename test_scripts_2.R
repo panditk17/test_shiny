6594718794ba0574f0864dca1a89d327cf98c885
Pandit <karunpandit@ufl.edu>
2022-06-03 02:22:48 -0400

    test two

---
+++ test_scripts_2.R
@@ -0,0 +1,25 @@
+library(shiny)
+# ui <- fluidPage(
+#   sliderInput(input="x", label = "If x is", min = 1, max = 50, value = 30),
+#   "then x times 5 is",
+#   textOutput("product")
+# )
+# 
+# server <- function(input, output, session) {
+#   output$product <- renderText({ 
+#     input$x * 5
+#   })
+# }
+
+
+ui <- fluidPage(
+  numericInput(inputId = "n",
+               "Sample size", value = 25),
+  plotOutput(outputId = "hist")
+)
+server <- function(input, output) { output$hist <- renderPlot({
+  hist(rnorm(input$n))
+})
+}
+
+shinyApp(ui, server)
