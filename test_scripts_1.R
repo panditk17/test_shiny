c4cc491633dab08c37cf8d8e2364ee24a679aa71
Pandit <karunpandit@ufl.edu>
2022-06-03 02:21:16 -0400

    new_trial_codes

--- test_scripts_1.R
+++ test_scripts_1.R
@@ -1,20 +1,27 @@
 ## codes to test r shiny app
 
 library(shiny)
-ui<-fluidPage("Hello World new")
+ui <- fluidPage(
+  sliderInput(input="x", label = "If x is", min = 1, max = 50, value = 30),
+  "then x times 5 is",
+  textOutput("product")
+)
 
-server<-fuction(input,output){}
+server <- function(input, output, session) {
+  output$product <- renderText({ 
+    input$x * 5
+  })
+}
 
+# 
+# ui <- fluidPage(
+#   numericInput(inputId = "n",
+#                "Sample size", value = 25),
+#   plotOutput(outputId = "hist")
+# )
+# server <- function(input, output) { output$hist <- renderPlot({
+#   hist(rnorm(input$n))
+# })
+# }
 
-## create a function
-disease_data<-c(1,1,2,2,3,6,5,3,2,nrow=3)
-predicted_hotspot_fn<-function(disease_data,predicted) 
-  { 
-  
-
-  
-
-shinyApp(ui=ui,server=server)
-
-
-
+shinyApp(ui, server)
