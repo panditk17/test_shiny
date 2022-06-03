702601bfb876d1e40372276e6d0a3e6a87ca2622
Pandit <karunpandit@ufl.edu>
2022-06-03 01:31:14 -0400

    new commits

--- test_scripts_1.R
+++ test_scripts_1.R
@@ -1,6 +1,20 @@
 ## codes to test r shiny app
 
 library(shiny)
-ui<-fluidPage("Hello World")
+ui<-fluidPage("Hello World new")
+
 server<-fuction(input,output){}
+
+
+## create a function
+disease_data<-c(1,1,2,2,3,6,5,3,2,nrow=3)
+predicted_hotspot_fn<-function(disease_data,predicted) 
+  { 
+  
+
+  
+
 shinyApp(ui=ui,server=server)
+
+
+
